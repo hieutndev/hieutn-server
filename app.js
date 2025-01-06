1d917cad11b1c57a93d45ec8e54319a5c7fcfece
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2025-01-06 22:28:22 +0700

    feat: add cors

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 
 const app = express();
+
+// app.use(cors({
+// 	"origin": "*",
+// 	"methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+// 	"preflightContinue": false,
+// }))
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,6 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/api', indexRouter);
 
