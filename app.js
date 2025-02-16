7709a3f86924a7560c7758aa88fe7044a81abc4e
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2025-02-16 18:22:59 +0700

    feat: transform socket event name to single file

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const debug = require('debug')('hieutn-server:server');
 const http = require("http");
 const GameCardService = require('./services/GameCardService');
-
+const SOCKET_EVENT_NAMES = require("./configs/socket-event-names");
 
 const indexRouter = require('./routes/index');
 
@@ -73,19 +73,19 @@ const io = new Server(server, {
 let currentInRooms = new Map();
 
 io.on('connection', (socket) => {
-	socket.on("createNewRoom", async ({ created_by, roomConfig }) => {
+	socket.on(SOCKET_EVENT_NAMES.CREATE_NEW_ROOM.RECEIVE, async ({ created_by, roomConfig }) => {
 		const createNewRoom = await GameCardService.createNewRoom(created_by, roomConfig);
 
 		if (createNewRoom.isCompleted) {
 			const getListRooms = await GameCardService.getAllRooms();
-			io.emit("roomCreated", {
+			io.emit(SOCKET_EVENT_NAMES.CREATE_NEW_ROOM.SEND, {
 				newRoomId: createNewRoom.results.newRoomId,
 				listRooms: getListRooms.results
 			});
 		}
 	})
 
-	socket.on("joinCGRoom", ({ roomId, username }) => {
+	socket.on(SOCKET_EVENT_NAMES.JOIN_CARDGAME_ROOM, ({ roomId, username }) => {
 		socket.join(roomId.toString());
 		if (!currentInRooms.get(roomId)) {
 			currentInRooms.set(roomId, [username]);
@@ -93,38 +93,38 @@ io.on('connection', (socket) => {
 			currentInRooms.set(roomId, Array.from(new Set([...currentInRooms.get(roomId), username])).filter((_v) => _v));
 		}
 
-		io.to(roomId.toString()).emit("playerChange", {
+		io.to(roomId.toString()).emit(SOCKET_EVENT_NAMES.PLAYER_CHANGE, {
 			currentInRoom: currentInRooms.get(roomId).length,
 			playersInRoom: currentInRooms.get(roomId),
 		});
 	})
 
-	socket.on("outCGRoom", ({ roomId, username }) => {
+	socket.on(SOCKET_EVENT_NAMES.LEAVE_CARDGAME_ROOM, ({ roomId, username }) => {
 		socket.leave(roomId.toString());
 		currentInRooms.set(roomId, currentInRooms.get(roomId) ? [...currentInRooms.get(roomId).filter((_v) => _v !== username)] : []);
 
-		io.to(roomId.toString()).emit("playerChange", {
+		io.to(roomId.toString()).emit(SOCKET_EVENT_NAMES.PLAYER_CHANGE, {
 			currentInRoom: currentInRooms.get(roomId).length,
 			playersInRoom: currentInRooms.get(roomId),
 		});
 	})
 
-	socket.on("createNewResult", async ({
-											roomId,
-											player1Result,
-											player2Result,
-											player3Result,
-											player4Result,
-											twoPlayResults,
-											createdBy,
-										}) => {
+	socket.on(SOCKET_EVENT_NAMES.CREATE_RESULT.RECEIVE, async ({
+																   roomId,
+																   player1Result,
+																   player2Result,
+																   player3Result,
+																   player4Result,
+																   twoPlayResults,
+																   createdBy,
+															   }) => {
 		const insertNewResult = await GameCardService.insertNewResult(roomId, player1Result, player2Result, player3Result, player4Result, twoPlayResults);
 
 		if (insertNewResult.isCompleted) {
 			const roomResults = await GameCardService.getRoomResults(roomId);
 
 			if (roomResults.isCompleted) {
-				io.to(roomId.toString()).emit("newResultCreated", {
+				io.to(roomId.toString()).emit(SOCKET_EVENT_NAMES.CREATE_RESULT.SEND, {
 					createdBy,
 					roomResults: roomResults.results
 				});
@@ -136,7 +136,7 @@ io.on('connection', (socket) => {
 		}
 
 	})
-	socket.on("updateRoomConfig", async ({ roomId, updatedBy, newConfig }) => {
+	socket.on(SOCKET_EVENT_NAMES.UPDATE_ROOM_CONFIG.RECEIVE, async ({ roomId, updatedBy, newConfig }) => {
 		const updateRoomConfig = await GameCardService.updateRoomConfig(roomId, newConfig);
 
 		if (updateRoomConfig.isCompleted) {
@@ -144,7 +144,7 @@ io.on('connection', (socket) => {
 
 			if (roomInfo.isCompleted) {
 
-				io.to(roomId.toString()).emit("roomConfigUpdated", {
+				io.to(roomId.toString()).emit(SOCKET_EVENT_NAMES.UPDATE_ROOM_CONFIG.SEND, {
 					updatedBy,
 					roomDetails: roomInfo.results
 				});
@@ -157,14 +157,14 @@ io.on('connection', (socket) => {
 		}
 	})
 
-	socket.on("deleteMatchResults", async ({ deleteBy, roomId, matchId }) => {
+	socket.on(SOCKET_EVENT_NAMES.DELETE_MATCH_RESULTS.RECEIVE, async ({ deleteBy, roomId, matchId }) => {
 		const deleteMatchResults = await GameCardService.deleteResults(roomId, matchId);
 
 		if (deleteMatchResults.isCompleted) {
 			const roomResults = await GameCardService.getRoomResults(roomId);
 
 			if (roomResults.isCompleted) {
-				io.to(roomId.toString()).emit("matchResultsDeleted", {
+				io.to(roomId.toString()).emit(SOCKET_EVENT_NAMES.DELETE_MATCH_RESULTS.SEND, {
 					deleteBy,
 					roomResults: roomResults.results
 				});
---
+++ configs/socket-event-names.js
@@ -0,0 +1,23 @@
+const SOCKET_EVENT_NAMES = {
+	UPDATE_ROOM_CONFIG: {
+		RECEIVE: "updateRoomConfig",
+		SEND: "roomConfigUpdated",
+	},
+	CREATE_RESULT: {
+		RECEIVE: "createNewResult",
+		SEND: "newResultCreated",
+	},
+	DELETE_MATCH_RESULTS: {
+		RECEIVE: "deleteMatchResults",
+		SEND: "matchResultsDeleted",
+	},
+	CREATE_NEW_ROOM: {
+		RECEIVE: "createNewRoom",
+		SEND: "roomCreated",
+	},
+	JOIN_CARDGAME_ROOM: "joinCGRoom",
+	LEAVE_CARDGAME_ROOM: "outCGRoom",
+	PLAYER_CHANGE: "playerChange",
+};
+
+module.exports = SOCKET_EVENT_NAMES;
